2e9595cec8366f2a28e1d1ffb8dd484f93e6aafb
Tyler Watson <tyler.watson@withreach.com>
2023-04-19 23:57:49 -0600

    Adding add to calendar button

--- src/components/BaseballGame.vue
+++ src/components/BaseballGame.vue
@@ -6,13 +6,29 @@
     <div class="card-body">
       <p><strong>Opponent:</strong> {{ game.opponent }}</p>
       <p><strong>Location:</strong> <a :href="googleMapsLink" target="_blank">{{ game.location }}</a></p>
+      <add-to-calendar-button
+          :name="`Baseball Game vs ${game.opponent}`"
+          options="'Apple','Google'"
+          :location="game.address"
+          :startDate="game.date"
+          :endDate="game.date"
+          :startTime="game.start_time"
+          :endTime="addHours(game.start_time, 1.5)"
+          timeZone="America/Edmonton"
+          :description="`Baseball Game vs ${game.opponent} at ${game.location}`"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import AddToCalendarButton from 'add-to-calendar-button'
+import moment from 'moment'
 export default {
   name: 'BaseballGame',
+  components: {
+    AddToCalendarButton
+  },
   props: {
     game: {
       type: Object,
@@ -29,6 +45,10 @@ export default {
       const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
       const ampm = hours >= 12 ? 'PM' : 'AM';
       return `${formattedHours}:${minutes} ${ampm}`;
+    },
+    addHours(timeString, hoursToAdd) {
+      const time = moment(timeString, 'HH:mm');
+      return time.add(hoursToAdd, 'hours').format('HH:mm');
     }
   },
   computed: {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import {createApp} from 'vue';
+import App from './App.vue';
+import router from './router';
+import {AddToCalendarButton} from 'add-to-calendar-button';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App).use(router);
+
+app.component('AddToCalendarButton', AddToCalendarButton);
+
+app.mount('#app');
