e8d04e0a5e540cd78f42991ba86cd6850659b4f8
Tyler Watson <tyler.watson@withreach.com>
2023-04-19 21:49:43 -0600

    Adding NavBar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,12 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <router-link to="/" class="navbar-brand">Bad News Beers</router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link to="/" class="nav-link">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/stats" class="nav-link">Stats</router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <NavBar/>
   <router-view/>
 </template>
 
 <style>
 @import '~bootstrap/dist/css/bootstrap.css';
 </style>
+<script setup>
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import NavBar from '@/components/NavBar.vue';
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="/" class="navbar-brand">Bad News Beers</router-link>
+    <button class="navbar-toggler" type="button" @click="toggleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" :class="{ 'show': isNavbarActive }">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/stats" class="nav-link">Stats</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isNavbarActive: false,
+    };
+  },
+  methods: {
+    toggleNavbar() {
+      this.isNavbarActive = !this.isNavbarActive;
+    },
+  },
+};
+</script>
